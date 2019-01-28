64eac72e88683fb64f764f72bd05bbd6caa9f285
NikhileshP <nikhileshhpandey@gmail.com>
2019-01-28 23:10:30 +0530

    updating files for new project Melange Meetups.

---
+++ server/routes/speakers/speakersIndex.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = () => {
+
+router.get('/', (req, res,next) => {
+
+    return res.send('All Speakers');
+});
+
+router.get('/:name', (req, res,next) => {
+
+    return res.send(`speaker detail page for ${req.params.name}`);
+});
+
+return router;
+
+};
